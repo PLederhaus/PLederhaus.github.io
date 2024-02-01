7ca6ab7ec1bc61a9630ffbac3884d3a185728412
Amelia <paytonlederhaus@gmail.com>
2024-01-31 22:03:35 -0600

    Added dynamic welcome

--- js/site.js
+++ js/site.js
@@ -1,1 +1,23 @@
-console.log("Whoa, I'm in the console!")
+const hours = new Date().getHours() // get the current hour
+const welcome = document.querySelector('#welcome')
+
+function greetByTime(hours){
+    const isMorning = hours >= 4 && hours < 12 // Good morning!
+    const isAfternoon = hours >= 12 && hours < 17 // Good afternoon!
+    const isEvening = hours >= 17 || hours < 4 // Good evening!
+
+    if (isMorning == true) {
+        welcome.textContent = "Good morning!"
+    }
+    
+    if (isAfternoon == true) {
+        welcome.textContent = "Good afternoon!"
+    }
+
+    if (isEvening == true) {
+        welcome.textContent = "Good evening!"
+    }
+}
+
+//Run function
+greetByTime(hours)
